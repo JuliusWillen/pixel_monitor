0ab873b3e1e52363777f60bab7d7ee8202c53cca
Julius Willén <69385664+JuliusWillen@users.noreply.github.com>
2022-10-01 13:23:15 +0200

    Update screen.py

--- screen.py
+++ screen.py
@@ -10,6 +10,8 @@ MAIL_SCREEN_CHANGED = (settings.TO_MAIL, "Screen changed!",
                        ["Screen has changed. Please check your computer. Script is paused for 60 seconds!"])
 MAIL_SESSION_STARTED = (settings.TO_MAIL, "Screen monitor session started!",
                         ["Screen is being monitored. You will receive a mail when the screen updates!"])
+MAIL_SESSION_STOPPED = (settings.TO_MAIL, "Screen monitor session stopped!",
+                        ["Something went wrong."])
 
 
 def send_mail(yag, mail):
@@ -32,15 +34,18 @@ def monitor_pixel(pos, yag):
     # get color of position
     color = pyautogui.pixel(pos.x, pos.y)
     print("Got color of chosen position: ", pos, color)
-    running = True
-    while running:
-        new_color = pyautogui.pixel(pos.x, pos.y)
-        print("Color on chosen position:", new_color)
-        time.sleep(3)  # wait for 3 seconds
-        if new_color != color:
-            print("Color mismatch: ", new_color, color)
-            send_mail(yag, MAIL_SCREEN_CHANGED)
-            time.sleep(60)
+    try:
+        while True:
+            new_color = pyautogui.pixel(pos.x, pos.y)
+            print("Color on chosen position:", new_color)
+            time.sleep(3)  # wait for 3 seconds
+            if new_color != color:
+                print("Color mismatch: ", new_color, color)
+                send_mail(yag, MAIL_SCREEN_CHANGED)
+                time.sleep(60)
+    except:
+        print("Something went wrong")
+        send_mail(yag, MAIL_SESSION_STOPPED)
 
 
 yag = yagmail.SMTP(settings.USERNAME, settings.PASSWORD)
